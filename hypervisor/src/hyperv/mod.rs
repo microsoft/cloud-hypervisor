477e0545cfe9e9937d9f468a4c0edf72ec783d3f
Wei Liu <liuwe@microsoft.com>
2020-07-17 08:47:18 +0000

    Drop some incorrect copyright information
    This file is created by Microsoft solely.
    
    Signed-off-by: Wei Liu <liuwe@microsoft.com>

--- hypervisor/src/hyperv/mod.rs
+++ hypervisor/src/hyperv/mod.rs
@@ -1,11 +1,6 @@
-// Copyright © 2019 Intel Coreoration
-//
 // SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 //
 // Copyright © 2020, Microsoft Corporation
-//
-// Copyright 2018-2019 CrowdStrike, Inc.
-//
 //
 
 use crate::cpu;
