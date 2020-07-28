40676ff58875d5aab4f4bbc56678902840555857
Wei Liu <liuwe@microsoft.com>
2020-07-28 16:16:37 +0000

    Disable warnings
    The Hyper-V module is work-in-progress. Disable a bunch of warnings to
    have less clutter.
    
    Signed-off-by: Wei Liu <liuwe@microsoft.com>

--- hypervisor/src/hyperv/mod.rs
+++ hypervisor/src/hyperv/mod.rs
@@ -3,6 +3,12 @@
 // Copyright © 2020, Microsoft Corporation
 //
 
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(unused_macros)]
+#![allow(non_upper_case_globals)]
+
 use crate::cpu;
 use crate::hypervisor;
 use crate::vm;
