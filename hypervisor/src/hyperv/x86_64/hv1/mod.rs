b416e69890dc5e01bc2e6e352d815ba559d7fe0c
Nuno Das Neves <nudasnev@microsoft.com>
2020-08-14 16:44:24 +0000

    Merged PR 3365111: Hardcoded TSC and APIC frequency MSRs
    Providing these allows booting a guest kernel without the TSC frequency hack we had to add previously.

--- hypervisor/src/hyperv/x86_64/hv1/mod.rs
+++ hypervisor/src/hyperv/x86_64/hv1/mod.rs
@@ -17,8 +17,15 @@ pub fn process_cpuid(rax: u32) -> (u32, u32, u32, u32) {
         HV_CPUID_FUNCTION_MS_HV_FEATURES => {
             let privileges = HV_PARTITION_PRIVILEGE_ACCESS_HYPERCALL_MSRS
                 | HV_PARTITION_PRIVILEGE_ACCESS_VP_INDEX
+                | HV_PARTITION_PRIVILEGE_ACCESS_FREQUENCY_MSRS
                 /*| HV_PARTITION_PRIVILEGE_PARTITION_REFERENCE_COUNTER*/;
-            (privileges as u32, (privileges >> 32) as u32, 0, 0)
+            let features = HV_FEATURE_FREQUENCY_REGS_AVAILABLE;
+            (
+                privileges as u32,
+                (privileges >> 32) as u32,
+                0,
+                features
+            )
         }
         HV_CPUID_FUNCTION_MS_HV_ENLIGHTENMENT_INFORMATION => (
             HV_ENLIGHTENMENT_DEPRECATE_AUTO_EOI,
@@ -36,6 +43,20 @@ pub fn process_msr_read(vp_index: u32, n: u32) -> Option<u64> {
         HV_X64_MSR_GUEST_OS_ID => 0,            // GUEST ID, meaning full ID
         HV_X64_MSR_HYPERCALL => 0,              // TODO
         HV_X64_MSR_VP_INDEX => vp_index as u64, // VP index
+        /*
+         * vcpu register for this is only available on ARM
+         * TODO better way to get this - e.g. cat /proc/cpuinfo
+         */
+        HV_X64_MSR_TSC_FREQUENCY => {
+            1000000000
+        },
+        /*
+         * vcpu register for this isn't available in 19h1
+         * TODO better way to get this
+         */
+        HV_X64_MSR_APIC_FREQUENCY => {
+            200000000
+        },
         _ => {
             debug!("Unhandled MSR read: {:x}", n);
             return None;
