8dee0349e947b180dcdb0f06f62a14beeb4e45a9
Jinank Jain <jinankjain@linux.microsoft.com>
2023-02-07 03:43:33 +0000

    Add support for loading IGVM file
    Signed-off-by: Jinank Jain <jinankjain@linux.microsoft.com>

--- arch/src/x86_64/mod.rs
+++ arch/src/x86_64/mod.rs
@@ -62,6 +62,8 @@ const KVM_FEATURE_STEAL_TIME_BIT: u8 = 5;
 pub struct EntryPoint {
     /// Address in guest memory where the guest must start execution
     pub entry_addr: Option<GuestAddress>,
+    #[cfg(feature = "igvm")]
+    pub vmsa: Option<SEV_VMSA>,
 }
 
 const E820_RAM: u32 = 1;
