0b59d8953b1cdceff8f5f132c9bf443d9c0bac88
Muminul Islam <muislam@microsoft.com>
2024-01-30 13:13:00 -0800

    hypervisor: mshv: snp_enabled
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- hypervisor/src/mshv/mod.rs
+++ hypervisor/src/mshv/mod.rs
@@ -253,9 +253,11 @@ impl hypervisor::Hypervisor for MshvHypervisor {
             break;
         }
 
+        let mut snp_enabled = false;
         // Set additional partition property for SEV-SNP partition.
         if mshv_vm_type == VmType::Snp {
             let snp_policy = snp::get_default_snp_guest_policy();
+            snp_enabled = true;
             let vmgexit_offloads = snp::get_default_vmgexit_offload_features();
             // SAFETY: access union fields
             unsafe {
