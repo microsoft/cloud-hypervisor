614a819ec47eb3dc438fb4a4f978f8e4cb3b1c65
Muminul Islam <muislam@microsoft.com>
2024-01-30 17:15:17 -0800

    hypervisor: use sev_snp feature gate instead of snp
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- hypervisor/src/kvm/mod.rs
+++ hypervisor/src/kvm/mod.rs
@@ -895,7 +895,7 @@ impl vm::Vm for KvmVm {
     fn as_any(&self) -> &dyn Any {
         self
     }
-    #[cfg(feature = "snp")]
+    #[cfg(feature = "sev_snp")]
     fn gain_page_access(&self, _gpa: u64, _size: u32) -> vm::Result<()> {
         Ok(())
     }
