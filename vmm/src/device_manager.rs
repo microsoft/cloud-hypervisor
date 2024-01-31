0b91d6e30c978bfb9400e66fe888e832854e4060
Muminul Islam <muislam@microsoft.com>
2024-01-30 17:14:50 -0800

    vmm, virtio-devices: Pass vmfd to console
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- vmm/src/device_manager.rs
+++ vmm/src/device_manager.rs
@@ -2128,6 +2128,8 @@ impl DeviceManager {
                 .map_err(DeviceManagerError::EventFd)?,
             versioned_state_from_id(self.snapshot.as_ref(), id.as_str())
                 .map_err(DeviceManagerError::RestoreGetState)?,
+            #[cfg(feature = "sev_snp")]
+            self.address_manager.vm.clone(),
         )
         .map_err(DeviceManagerError::CreateVirtioConsole)?;
         let virtio_console_device = Arc::new(Mutex::new(virtio_console_device));
