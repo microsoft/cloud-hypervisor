a80fe8591cfea70552b6ab6dfa3eb64815effde4
Muminul Islam <muislam@microsoft.com>
2024-02-01 16:09:58 -0800

    vmm, virtio-devices: clone quire with acquiring access
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- vmm/src/device_manager.rs
+++ vmm/src/device_manager.rs
@@ -3100,6 +3100,8 @@ impl DeviceManager {
                     .map_err(DeviceManagerError::EventFd)?,
                 versioned_state_from_id(self.snapshot.as_ref(), id.as_str())
                     .map_err(DeviceManagerError::RestoreGetState)?,
+                #[cfg(feature = "sev_snp")]
+                self.address_manager.vm.clone(),
             )
             .map_err(DeviceManagerError::CreateVirtioVsock)?,
         ));
@@ -3850,6 +3852,8 @@ impl DeviceManager {
                 dma_handler,
                 self.pending_activations.clone(),
                 vm_migration::snapshot_from_id(self.snapshot.as_ref(), id.as_str()),
+                #[cfg(feature = "sev_snp")]
+                self.address_manager.vm.clone(),
             )
             .map_err(DeviceManagerError::VirtioDevice)?,
         ));
