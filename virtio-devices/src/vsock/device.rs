1c615e9545fabd19946e2dde58601362627ff0de
Muminul Islam <muislam@microsoft.com>
2024-02-01 16:09:58 -0800

    vmm, virtio-devices: clone quire with acquiring access
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- virtio-devices/src/vsock/device.rs
+++ virtio-devices/src/vsock/device.rs
@@ -99,6 +99,8 @@ pub struct VsockEpollHandler<B: VsockBackend> {
     pub interrupt_cb: Arc<dyn VirtioInterrupt>,
     pub backend: Arc<RwLock<B>>,
     pub access_platform: Option<Arc<dyn AccessPlatform>>,
+    #[cfg(feature = "sev_snp")]
+    pub vm: Arc<dyn hypervisor::Vm>,
 }
 
 impl<B> VsockEpollHandler<B>
@@ -131,6 +133,8 @@ where
             let used_len = match VsockPacket::from_rx_virtq_head(
                 &mut desc_chain,
                 self.access_platform.as_ref(),
+                #[cfg(feature = "sev_snp")]
+                self.vm.clone(),
             ) {
                 Ok(mut pkt) => {
                     if self.backend.write().unwrap().recv_pkt(&mut pkt).is_ok() {
@@ -173,6 +177,8 @@ where
             let pkt = match VsockPacket::from_tx_virtq_head(
                 &mut desc_chain,
                 self.access_platform.as_ref(),
+                #[cfg(feature = "sev_snp")]
+                self.vm.clone(),
             ) {
                 Ok(pkt) => pkt,
                 Err(e) => {
@@ -321,6 +327,8 @@ pub struct Vsock<B: VsockBackend> {
     path: PathBuf,
     seccomp_action: SeccompAction,
     exit_evt: EventFd,
+    #[cfg(feature = "sev_snp")]
+    vm: Arc<dyn hypervisor::Vm>,
 }
 
 #[derive(Versionize)]
@@ -347,6 +355,7 @@ where
         seccomp_action: SeccompAction,
         exit_evt: EventFd,
         state: Option<VsockState>,
+        #[cfg(feature = "sev_snp")] vm: Arc<dyn hypervisor::Vm>,
     ) -> io::Result<Vsock<B>> {
         let (avail_features, acked_features, paused) = if let Some(state) = state {
             info!("Restoring virtio-vsock {}", id);
@@ -377,6 +386,8 @@ where
             path,
             seccomp_action,
             exit_evt,
+            #[cfg(feature = "sev_snp")]
+            vm,
         })
     }
 
@@ -461,6 +472,8 @@ where
             interrupt_cb,
             backend: self.backend.clone(),
             access_platform: self.common.access_platform.clone(),
+            #[cfg(feature = "sev_snp")]
+            vm: self.vm.clone(),
         };
 
         let paused = self.common.paused.clone();
